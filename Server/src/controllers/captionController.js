cb34b2e717dbfce19997a96244c6c58b6eb1b2e6
Keerthana277-at <vkeerty7@gmail.com>
2026-07-27 20:17:54 +0530

    Completed fetch and delete operations

--- Server/src/controllers/captionController.js
+++ Server/src/controllers/captionController.js
@@ -37,4 +37,56 @@ async function createCaptions(req,res) {
     
 }
 
-module.exports = { createCaptions };
+async function getCaptions(req,res) {
+    try{
+        const captions = await Caption.find({
+            user: req.user.id
+        }).sort({
+            createdAt: -1
+        });
+        
+        return res.status(200).json({
+            success:"true",
+            message:"Captions fetched successfully",
+            captions
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message:"Internal server error"
+        })
+    }
+}
+
+async function deleteCaptions(req,res) {
+    try{
+        const captions = await Caption.findById(req.params.id);
+        if(!captions)
+            return res.status(404).json({
+                message:"Caption doesn't exist"
+        })
+        if(captions.user.toString() !== req.user.id){
+            
+            return res.status(403).json({
+                message:"You're not authorized to delete this caption"
+            });
+        }
+        
+           await Caption.findByIdAndDelete(captions._id); 
+            return res.status(200).json({
+                message:"Caption Deleted Successfully"
+            });
+        
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message:"Internal server error"
+        });
+    }      
+          
+}
+
+module.exports = { createCaptions,
+    getCaptions,
+    deleteCaptions
+ };
--- Server/src/routes/captionRoutes.js
+++ Server/src/routes/captionRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createCaptions } = require("../controllers/captionController");
+const { createCaptions,getCaptions,deleteCaptions } = require("../controllers/captionController");
 const authMiddleware = require("../middleware/authMiddleware"); 
 router.post("/",authMiddleware,createCaptions);
+router.delete("/:id",authMiddleware,deleteCaptions);
+
+router.get("/",authMiddleware,getCaptions);
 
 
 module.exports = router;
