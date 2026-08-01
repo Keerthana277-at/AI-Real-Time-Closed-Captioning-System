731ae8cab2267592081d57f026dcac52217646e7
Keerthana277-at <vkeerty7@gmail.com>
2026-08-01 21:54:45 +0530

    Finished original speech to simplified speech as text through audio files

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+app.use(express.static(path.join(__dirname,"..")));
 app.use(express.json());
 const speechRoutes = require("./routes/speechRoutes");
 const userRoutes = require("./routes/userRoutes");
--- Server/src/controllers/speechController.js
+++ Server/src/controllers/speechController.js
@@ -1,13 +1,20 @@
 const { speechToText } = require("../services/speechService");
-
+const aiService = require("../services/aiService");
+console.log("AI Service:",aiService);
 async function transcribeSpeech(req,res) {
     try{
+
         const file = req.file;
+        console.log("TRANSCRIBE CONTROLLER CALLED");
+
+        console.log("FILE:", req.file);
         const text = await speechToText(file);
+        const simplifiedText = await aiService.simplifyText(text);
         return res.status(200).json({
             success:true,
             message:"Transcribed successfully",
-            file : file
+            text : text,
+            simplifiedText:simplifiedText
         }); 
     }catch(error){
         console.log(error);
